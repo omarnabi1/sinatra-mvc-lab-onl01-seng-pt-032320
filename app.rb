533d0cb63d47fe1e346c93247f0ae6c544fb171b
Omar Nabi <onabi20@gmail.com>
2020-06-21 16:24:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,6 +3,16 @@ require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
   
+   get '/' do 
+    
+    erb :user_input
+  end
+  
+  post '/piglatinize' do
+    pl = PigLatinizer.new
+    @piglatin = pl.piglatinize(params["user_phrase"])
+    erb :results
+  end
 
 
   
