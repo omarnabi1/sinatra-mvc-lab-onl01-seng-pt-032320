612f4387cd0dc34bee4d95de479c65e37efc5373
Omar Nabi <onabi20@gmail.com>
2020-06-21 16:22:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get "/" do 
+  end
+  
+  
+  post "/piglatinize" do 
+  end
+  
+  post "/piglatinize"
+  end
+
+  
+  
 end
